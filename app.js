f8f463f38d3212a871a5323a6dc6e93f9ca90f58
fabianburgos14 <fabianansiolitiko@gmail.com>
2024-05-23 01:34:12 -0500

    S2

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/contacto', (req, res) => {
+    res.render('contacto');
+});
+
+app.get('/productos', (req, res) => {
+    res.render('productos');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
